fe784263616a265b9a7236e99ef5f5d987b4c0f2
Rustam <rustam5491@gmaill.com>
2023-03-01 12:39:05 +0300

    Фидбек

--- pythonProject12/main.py
+++ pythonProject12/main.py
@@ -5,14 +5,14 @@ print("Последовательность для сравнения:", a)
 
 print("Введите последовательность для сортировки:")
 b = list(map(int, input().split()))
-N = len(b)
-for i in range(1, N):
-    for j in range(i, 0, -1):
-        if b[j] < b[j-1]:
-            b[j],b[j-1]= b[j-1],b[j]
-        else:
-            break
-    print(b)
+# N = len(b)
+# for i in range(1, N):
+#     for j in range(i, 0, -1):
+#         if b[j] < b[j-1]:
+#             b[j],b[j-1]= b[j-1],b[j]
+#         else:
+#             break
+#     print(b)
 
 print("Последовательность после сортировки:", b)
 
